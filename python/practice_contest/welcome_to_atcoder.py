1203e0b7c576684c137c243d4a243924abb5e135
yss <waku.pdxppxzy@gmail.com>
2023-06-13 01:02:09 +0900

    add: practice/welcome_to_atcoder

---
+++ python/practice_contest/welcome_to_atcoder.py
@@ -0,0 +1,10 @@
+# https://atcoder.jp/contests/practice/tasks/practice_1
+
+def main():
+    a = int(input())
+    b, c = map(int, input().split(" "))
+    s = input()
+    print(f"{a + b + c} {s}")
+
+if __name__ == "__main__":
+    main()
