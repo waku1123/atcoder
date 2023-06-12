85623724b82b8da038f200fb93033ac6a7401193
yss <waku.pdxppxzy@gmail.com>
2023-06-13 03:09:32 +0900

    add: abc086a_product
    https://atcoder.jp/contests/abs/tasks/abc086_a

---
+++ python/beginners_selection/abc086a_product.py
@@ -0,0 +1,10 @@
+import math
+
+
+def main():
+    # product = math.prod(list(map(int, input().split(" "))))
+    print("Even") if math.prod(list(map(int, input().split(" ")))) % 2 == 0 else print("Odd")
+
+
+if __name__ == "__main__":
+    main()
