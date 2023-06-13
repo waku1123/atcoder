6299f628e1cb4f4926e5afe3ab6c41ef16c572e2
yss <waku.pdxppxzy@gmail.com>
2023-06-14 01:07:32 +0900

    add: abc049c_day_dream
    https://atcoder.jp/contests/abs/tasks/arc065_a

---
+++ python/beginners_selection/abc049c_day_dream.py
@@ -0,0 +1,9 @@
+def main():
+    templates = ["eraser", "erase", "dreamer", "dream"]
+    S = input()
+    for template in templates:
+        S = S.replace(template, "")
+    print("YES") if S == "" else print("NO")
+
+if __name__ == "__main__":
+    main()
