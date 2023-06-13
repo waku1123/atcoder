38d13aa52fff724ec3e40d7f2e61309deb52daf9
yss <waku.pdxppxzy@gmail.com>
2023-06-14 00:53:15 +0900

    update: abc087b_coins

--- python/beginners_selection/abc087b_coins.py
+++ python/beginners_selection/abc087b_coins.py
@@ -4,12 +4,6 @@ def main():
     C = int(input())
     X = int(input())
     count = 0
-    # a_amounts = [500 * i for i in range(1, a+1)] if a else []
-    # b_amounts = [100 * j for j in range(1, b+1)] if b else []
-    # c_amounts = [50 * k for k in range(1, c+1)] if c else []
-    # print(a_amounts)
-    # print(b_amounts)
-    # print(c_amounts)
     for a in range(A + 1):
         for b in range(B + 1):
             for c in range(C + 1):
